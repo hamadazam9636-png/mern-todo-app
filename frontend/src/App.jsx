e590fd946ea25c06661915df21f933eb56d66273
CodewithHadi <hamadazam9636@gmail.com>
2026-07-19 10:06:11 -0700

    new

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,11 +9,38 @@ import SignUp from './components/SignUp'
 import Login from './components/Login'
 import Protected from './components/Protected'
 import { PopupProvider } from "./components/PopupContext"
+import axios from 'axios';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+axios.interceptors.request.use(
+  (config) => {
+    if (!navigator.onLine) {
+      toast.error("No internet connection. Please check your network status.");
+      return Promise.reject(new Error("No internet connection"));
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.message === "Network Error" || !navigator.onLine) {
+      toast.error("Network connection lost. Please try again.");
+    }
+    return Promise.reject(error);
+  }
+);
 function App() {
 
 
   return (
     <>
+    <ToastContainer position="top-right" autoClose={3000} />
     <PopupProvider>
     <Navbar/>
     <Routes>
